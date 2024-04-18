9c7a7125dbf2b246c867961afe99a23905434479
chikuwa-ah <chikuwah21@gmail.com>
2024-04-18 23:55:28 +0900

    judge

--- js/main.js
+++ js/main.js
@@ -4,8 +4,7 @@ let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 
 let end = true, flashId;
-let level = 0, i = -1;
-let alphabet_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let level, score, point, times, great, miss, display_time;
 
 const letter = [];
 for (let im = 0; im < 26; im++) {
@@ -25,7 +24,7 @@ let alphabet = [];
 
 function alphabetAdd() {
     let a_x = 0;
-    let a_dx = 1;
+    let a_dx = 5;
     let a_word = Math.floor(Math.random() * 26);
 
     let a = new Alphabet(a_x, a_dx, a_word);
@@ -38,25 +37,42 @@ function alphabetAdd() {
 let test_x = 0;
 
 function main() {
-    let id;
-
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (let i = 0; i < alphabet.length; i++) {
-        ctx.drawImage(letter[alphabet[i].word], alphabet[i].x, 270);
 
-        alphabet[i].x += alphabet[i].dx;
-        if (alphabet[i].x > canvas.width) {
+
+    if (alphabet.length > 0) {
+        for (let i = 0; i < alphabet.length; i++) {
+            ctx.drawImage(letter[alphabet[i].word], alphabet[i].x, 270);
+
+            alphabet[i].x += alphabet[i].dx;
+        };
+        if (alphabet[0].x > canvas.width) {
             alphabet.shift();
-        }
+        };
     };
 
-
-    style_settings('#fff', null, 'bold 45px sans-serif');
-    ctx.fillText('SCORE：', 50, canvas.height - 25);
-    ctx.fillText('Lv.' + level, 530, 70);
+    style_settings('#fff', '#fff', 'bold 20px sans-serif');
+    if (display_time > 0) {
+        display_time--;
+        let textWidth = ctx.measureText(great).width;
+        ctx.fillText(great, (425 - textWidth) / 2 + 600, 400);
+    }
 
     console.log('main');
+
+    ctx.font = 'bold 45px sans-serif';
+    ctx.fillText('SCORE：' + score, 50, canvas.height - 25);
+    ctx.fillText('Lv.' + level, 530, 70);
+    ctx.strokeRect(700, 30, 250, 45);
+    ctx.fillStyle = '#55f';
+    ctx.fillRect(770, 350, 30, 5);
+    ctx.fillRect(830, 350, 30, 5);
+    ctx.fillStyle = '#f55';
+    ctx.fillRect(800, 350, 30, 5);
+
+
+    let id;
     if (end) {
         window.cancelAnimationFrame(id);
         start();
@@ -64,6 +80,28 @@ function main() {
         id = window.requestAnimationFrame(main);
     };
 };
+
+function judge(a) {
+
+    if (a == alphabet[0].word) {
+
+        display_time = 20;
+        if (alphabet[0].x >= 770 && alphabet[0].x <= 860) {
+
+            great = 'GREAT!  ×1.2';
+
+            if (alphabet[0].x >= 800 && alphabet[0].x <= 830) {
+
+                great = 'PERFECT!!  ×1.5';
+            }
+
+        } else {
+            great = 'NICE ×1.0';
+        }
+    } else {
+        miss--;
+    }
+}
 
 
 
@@ -87,12 +125,27 @@ function keyDownHandler(e) {
     if (e.keyCode == 13 && end == false) {
         alphabetAdd();
     };
+
+
+    if (e.keyCode >= 65 && e.keyCode <= 90 && end == false) {
+        let key = e.keyCode - 65;
+        judge(key);
+    }
 };
+
+
+
+
 
 
 start();
 
 function start() {
+    //initialize
+    alphabet = [];
+    score = 0, level = 0, point = 0, times = 2, miss = 3, display_time = 0;
+
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     style_settings('#fff', null, 'bold 80px sans-serif');
